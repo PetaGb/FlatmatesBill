5b63aa0f27a9396528cc8c49b2c87a47cd6f3ab1
PetaGb <99599006+PetaGb@users.noreply.github.com>
2023-01-13 16:46:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+import webbrowser
+from fpdf import FPDF
+
+
+class Bill:
+    """
+    Objects contains data about a bill, such as
+    total amount and period of the bill
+    """
+
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    creates a flatmate person who lives in the flat
+    and pays a share of the bill
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
+
+
+class PdfReport:
+    """
+    Create a PDF file that creates  data about
+    the flatmates such as their names their due
+    amount and the period of the bill.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        pdf.image("house.png", w=50, h=50)
+
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates bill", border=0, align='C', ln=1)
+
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
+        pdf.output(self.filename)
+        webbrowser.open(self.filename)
+
+
+the_bill = Bill(amount=120, period="April 2021")
+John = Flatmate(name="John", days_in_house=20)
+Marry = Flatmate(name="Mary", days_in_house=25)
+
+print(John.pays(bill=the_bill, flatmate2=Marry))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=John, flatmate2=Marry, bill=the_bill)
+
