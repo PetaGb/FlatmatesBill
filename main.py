76410fdf0dfa3133c4503423229da69ae16a1b21
PetaGb <peter.gelec@gmail.com>
2023-01-25 15:23:09 +0100

    Refactoring/creating empty classes for in order to implement web interface

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from flat import Bill, Flatmate
-from report import PdfReport
-
-amount = float(input("Hey user, enter the bill amount: "))
-period = input("What is the bill period, e.g. December 2020: ")
-
-name1 = input("Whats your name? ")
-days_in_house1 = int(input(f"How many days did {name1} stayed in house during the period? "))
-name2 = input("Whats your mate's name? ")
-days_in_house2 = int(input(f"How many days did {name2} stayed in house during the period? "))
-
-the_bill = Bill(amount, period)
-flatmate1 = Flatmate(name1, days_in_house1)
-flatmate2 = Flatmate(name2, days_in_house2)
+from flask.views import MethodView
+from wtforms import form
 
 
-pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
-pdf_report.generate(flatmate1, flatmate2, the_bill)
+class HomePage(MethodView):
+    pass
+
+
+class BillFormPage(MethodView):
+    pass
+
+
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(form):
+    pass
